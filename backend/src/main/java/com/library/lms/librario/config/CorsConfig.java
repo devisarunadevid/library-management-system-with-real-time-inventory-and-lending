985fbee54f8b9f845c6233cb6a7a5689b031fd8e
Devis Aruna Devi D <23cseb04.devisarunadevi.d@gmail.com>
2026-03-22 21:26:39 +0530

    Update CORS configuration to allow all paths

--- backend/src/main/java/com/library/lms/librario/config/CorsConfig.java
+++ backend/src/main/java/com/library/lms/librario/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
                                 "https://library-management-system-with-real.vercel.app"
