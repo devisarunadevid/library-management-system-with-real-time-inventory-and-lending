9a6f8728305b52fb51f7b94f44d7fde22c3cfe2d
Devis Aruna Devi D <23cseb04.devisarunadevi.d@gmail.com>
2026-03-23 19:01:35 +0530

    Remove CrossOrigin annotation from UserController
    Removed CrossOrigin annotation for UserController.

--- backend/src/main/java/com/library/lms/librario/controller/UserController.java
+++ backend/src/main/java/com/library/lms/librario/controller/UserController.java
@@ -7,10 +7,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {
-    "http://localhost:5173",
-    "https://library-management-system-with-real.vercel.app"
-})
 public class UserController {
 
     private final UserRepository userRepo;
